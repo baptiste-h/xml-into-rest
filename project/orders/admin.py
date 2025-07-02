e58b8a1819b52fa1d3a5f65ce5938481e3c387b5
Baptiste H <baptiste.hetet@gmail.com>
2025-07-02 17:37:25 +0200

    Orders model

--- project/orders/admin.py
+++ project/orders/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from orders.models import Order
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("order_id", "marketplace", "order_status", "order_purchase_date")
+
+
+admin.site.register(Order, OrderAdmin)
--- project/orders/models.py
+++ project/orders/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    class Status(models.TextChoices):
+        NEW = "new"
+        PROCESSING = "processing"
+        SHIPPED = "shipped"
+        CANCEL = "cancel"
+
+    class Marketplace(models.TextChoices):
+        AMAZON = "amazon"
+        CDISCOUNT = "cdiscount"
+
+    order_id = models.CharField(max_length=20)
+    order_status = models.CharField(choices=Status.choices, max_length=20, null=True)
+    order_purchase_date = models.DateField(null=True)
+    order_amount = models.FloatField()
+    order_shipping = models.FloatField()
+    marketplace = models.CharField(choices=Marketplace.choices, max_length=20)
+
+    def __str__(self):
+        return f"{self.order_id}"
